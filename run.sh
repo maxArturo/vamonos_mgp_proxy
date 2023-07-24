a2ae2773bab6a10378004dbb7b4162612099c224
Max Alcala <maxarturo@gmail.com>
2023-07-24 11:38:06 -0300

    feat: add build id header

--- run.sh
+++ run.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 docker build -t test .
+
+# shellcheck source=/dev/null
 source .env
 
 docker run -it -p8080:8080 \
-    -e MGP_API_URL=$MGP_API_URL \
-    -e OSM_API_URL=$OSM_API_URL \
-    -e HOST_URL=$HOST_URL \
-    -e API_TOKEN=$API_TOKEN \
+    -e MGP_API_URL="$MGP_API_URL" \
+    -e OSM_API_URL="$OSM_API_URL" \
+    -e HOST_URL="$HOST_URL" \
+    -e API_TOKEN="$API_TOKEN" \
     test
