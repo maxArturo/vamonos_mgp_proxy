9175ef640e26a4c9e2537ed3e1d1250bf58eee80
Max Alcala <maxarturo@gmail.com>
2023-08-01 13:40:47 -0300

    wip: working redirects

---
+++ openresty/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+docker build -t proxy .
+
+# shellcheck source=/dev/null
+source .env
+
+docker run -p8080:80 \
+    -e MGP_API_URL="$MGP_API_URL" \
+    -e OSM_API_URL="$OSM_API_URL" \
+    -e HOST_URL="$HOST_URL" \
+    -e API_TOKEN="$API_TOKEN" \
+    proxy
