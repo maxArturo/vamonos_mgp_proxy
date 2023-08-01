76652db1eb39425a2d9a1260d4c38eefac4029de
Max Alcala <maxarturo@gmail.com>
2023-08-01 15:04:39 -0300

    fix: prom metrics

--- openresty/scripts/check_api_key.lua
+++ openresty/scripts/check_api_key.lua
@@ -1,9 +1,15 @@
+local function isempty(s)
+    return s == nil or s == ''
+end
+
 local api_key = ngx.req.get_headers()['X-Auth-Token']
-if api_key ~= os.getenv("API_TOKEN") then
-    -- ngx.log(ngx.ALERT, "api token is: ")
-    -- ngx.log(ngx.ALERT, os.getenv("API_TOKEN"))
+local build_id = ngx.req.get_headers()['X-Build-Id']
+if api_key ~= os.getenv("API_TOKEN") or isempty(build_id) then
     ngx.exit(ngx.HTTP_UNAUTHORIZED)
 end
+
+-- make sure to log the build ids
+metric_build_id:inc(1, {ngx.req.get_headers()['X-Build-Id'], ngx.var.upstream_provider})
 
 -- clear headers here
 headers = {'X-Forwarded-By', 'Via', 'Fly', 'X-Auth-Token', 'X-Build-Id', 'Cache-Congrol', 'pragma'}
